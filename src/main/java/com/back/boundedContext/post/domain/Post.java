4d480c6c32fc6d5d6b2eb92b6852e4cef4f2fe15
jieunsse <jieunsse@gmail.com>
2026-09-18 11:11:58 +0900

    15번 커밋

--- src/main/java/com/back/boundedContext/post/domain/Post.java
+++ src/main/java/com/back/boundedContext/post/domain/Post.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,7 @@ import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
+@Table(name = "POST_POST")
 @NoArgsConstructor
 @Getter
 public class Post extends BaseIdAndTime {
--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,9 +1,7 @@
-
-package com.back.boundedContext.member.domain;
-
-
+package com.back.boundedContext.post.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,23 +9,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "MEMBER_MEMBER")
+@Table(name = "POST_MEMBER")
 @NoArgsConstructor
 @Getter
-public class Member extends BaseIdAndTime {
+public class PostMember extends BaseIdAndTime {
 	@Column(unique = true)
 	private String username;
 	private String password;
 	private String nickname;
 	private int activityScore;
-
-	public Member(String username, String password, String nickname) {
-		this.username = username;
-		this.password = password;
-		this.nickname = nickname;
-	}
-
-	public int increaseActivityScore(int amount) {
-		return this.activityScore += amount;
-	}
 }

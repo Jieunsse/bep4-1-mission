1c5bce951fdedf90e3ac0a6e0b139fc13d095d0f
jieunsse <jieunsse@gmail.com>
2026-09-18 16:50:42 +0900

    24번 커밋

--- src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
+++ src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
@@ -4,13 +4,26 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.EntityListeners;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
+@NoArgsConstructor
 public abstract class BaseIdAndTimeManual extends BaseEntity {
 	@Id
 	private int id;
+	@CreatedDate
 	private LocalDateTime createDate;
+	@LastModifiedDate
 	private LocalDateTime modifyDate;
+
+	public BaseIdAndTimeManual(int id) {
+		this.id = id;
+	}
 }

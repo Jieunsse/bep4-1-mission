30665543072c985c484335b030299b30027bd96e
jieunsse <jieunsse@gmail.com>
2026-09-18 09:47:36 +0900

    12번 커밋

--- src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
+++ src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
@@ -3,6 +3,8 @@ package com.back.boundedContext.member.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.exception.DomainException;
+import com.back.global.rsData.RsData;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class MemberJoinUseCase {
 	private final MemberRepository memberRepository;
 
-	public Member join(String username, String password, String nickname) {
+	public RsData<Member> join(String username, String password, String nickname) {
 		memberRepository.findByUsername(username).ifPresent(m -> {
 			throw new DomainException("409-1", "이미 존재하는 username 입니다.");
 		});

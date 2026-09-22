345885ac3e88a655ce1b2af82b6c2edea42c5550
jieunsse <jieunsse@gmail.com>
2026-09-22 16:03:32 +0900

    0042 payout 정산서 생성 구현

--- src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
@@ -1,13 +1,21 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.Payout;
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import com.back.boundedContext.payout.out.PayoutRepository;
 import com.back.shared.payout.dto.PayoutMemberDto;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
+@RequiredArgsConstructor
 public class PayoutCreatePayoutUseCase {
-	public void createPayout(PayoutMemberDto payee) {
-		log.debug("createPayout.payee: {}", payee.getId());
+	private final PayoutRepository payoutRepository;
+	private final PayoutMemberRepository payoutMemberRepository;
+
+	public Payout createPayout(PayoutMemberDto payee) {
+		PayoutMember payoutMember = payoutMemberRepository.getReferenceById(payee.getId());
+		return payoutRepository.save(new Payout(payoutMember));
 	}
 }

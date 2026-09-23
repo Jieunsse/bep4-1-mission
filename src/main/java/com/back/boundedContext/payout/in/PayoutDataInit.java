b818aeb55313d6d25e4e7d5460b0ad8eb7780acb
jieunsse <jieunsse@gmail.com>
2026-09-23 12:43:27 +0900

    0049 정산 수집과 집행 배치 통합

--- src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
+++ src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
@@ -30,18 +30,18 @@ public class PayoutDataInit {
 	private final PayoutDataInit self;
 	private final PayoutFacade payoutFacade;
 	private final JobOperator jobOperator;
-	private final Job payoutCollectItemsJob;
+	private final Job payoutCollectItemsAndCompletePayoutsJob;
 
 	public PayoutDataInit(
 		@Lazy PayoutDataInit self,
 		PayoutFacade payoutFacade,
 		JobOperator jobOperator,
-		Job payoutCollectItemsJob
+		Job payoutCollectItemsAndCompletePayoutsJob
 	) {
 		this.self = self;
 		this.payoutFacade = payoutFacade;
 		this.jobOperator = jobOperator;
-		this.payoutCollectItemsJob = payoutCollectItemsJob;
+		this.payoutCollectItemsAndCompletePayoutsJob = payoutCollectItemsAndCompletePayoutsJob;
 	}
 
 	@Bean
@@ -50,8 +50,8 @@ public class PayoutDataInit {
 		return args -> {
 			self.forceMakePayoutReadyCandidatesItems();
 			self.collectPayoutItemsMore();
-			self.runCollectPayoutItemsBatchJob();
 			self.completePayoutsMore();
+			self.runCollectItemsAndCompletePayoutsBatchJob();
 		};
 	}
 
@@ -65,23 +65,21 @@ public class PayoutDataInit {
 
 	@Transactional
 	public void collectPayoutItemsMore() {
-		payoutFacade.collectPayoutItemsMore(4);
+		payoutFacade.collectPayoutItemsMore(2);
 	}
 
 	@Transactional
 	public void completePayoutsMore() {
-		payoutFacade.completePayoutsMore(4);
-		payoutFacade.completePayoutsMore(2);
-		payoutFacade.completePayoutsMore(2);
+		payoutFacade.completePayoutsMore(1);
 	}
 
-	public void runCollectPayoutItemsBatchJob() {
+	public void runCollectItemsAndCompletePayoutsBatchJob() {
 		JobParameters jobParameters = new JobParametersBuilder()
 			.addString("runDate", LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE))
 			.toJobParameters();
 
 		try {
-			JobExecution execution = jobOperator.start(payoutCollectItemsJob, jobParameters);
+			JobExecution execution = jobOperator.start(payoutCollectItemsAndCompletePayoutsJob, jobParameters);
 		} catch (JobInstanceAlreadyCompleteException e) {
 			log.error("Job instance already complete", e);
 		} catch (JobExecutionAlreadyRunningException e) {
